af4e366a78d39b86a13217932893768e6272fb3c
PatrickRenovato <patrick@sdwc.me>
2023-11-07 15:14:02 -0300

    test init parameter

--- src/index.js
+++ src/index.js
@@ -1,46 +1,57 @@
 let pageToken;
 let trackPageview = false;
 let trackFirstEventInterval = false;
-
 let initTime = false;
+
+let urlEventTrack = 'https://events.sdwc.me';
 
 export const track = function track(event, objectId, extraObj) {
     console.log(event);
     console.log(objectId);
     console.log(extraObj);
-    console.log(pageToken);
 
-    console.log(trackFirstEventInterval);
+    let diffSeconds = 0;
+
     if(trackFirstEventInterval) {
 
         const diffTime = Math.abs(initTime - new Date());
-        const diffSeconds = Math.ceil(diffTime / (1000)); 
-
-        console.log(diffSeconds);
-
+        diffSeconds = Math.ceil(diffTime / (1000)); 
         trackFirstEventInterval = false;
     }
+
+    sendTrack();
 };
 
 export const init = function init(token, extraObj) {
     pageToken = token;
-    console.log(extraObj);
 
     if(extraObj != null) {
         trackPageview = extraObj.track_pageview != undefined ? extraObj.track_pageview : false;
         trackFirstEventInterval = extraObj.track_firsteventinterval != undefined ? extraObj.track_firsteventinterval : false;
 
-        console.log(trackFirstEventInterval);
+        if(trackFirstEventInterval) {
+            initTime = new Date();
+        }
     }
 
-    afterInit();
+    sendPageHitTrack(token);
 };
 
-function afterInit() {
-    console.log("afterInit");
-    if(trackFirstEventInterval) {
-        initTime = new Date();
-        console.log(initTime);
-    }
+function sendTrack(token) {
+
 }
+
+function sendPageHitTrack(token) {
+
+    const params = new URLSearchParams({
+        event: 'page-hit',
+        token: token
+    });
+
+    let url = `${urlEventTrack}?${params.toString()}`;
+
+    console.log(url);
+}
+
+
 
