7b43cd193a4a799a5a3b7b9f1c01f45a33cdbc47
PatrickRenovato <patrick@sdwc.me>
2023-11-07 15:14:23 -0300

    commit build

--- dist/agm-awesome-utils.umd.js
+++ dist/agm-awesome-utils.umd.js
@@ -4,43 +4,29 @@
     (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global['swes-js-sdk'] = {}));
 }(this, (function (exports) { 'use strict';
 
-    var pageToken;
-    var trackFirstEventInterval = false;
-    var initTime = false;
+    var urlEventTrack = 'https://events.sdwc.me';
     var track = function track(event, objectId, extraObj) {
       console.log(event);
       console.log(objectId);
       console.log(extraObj);
-      console.log(pageToken);
-      console.log(trackFirstEventInterval);
-
-      if (trackFirstEventInterval) {
-        var diffTime = Math.abs(initTime - new Date());
-        var diffSeconds = Math.ceil(diffTime / 1000);
-        console.log(diffSeconds);
-        trackFirstEventInterval = false;
-      }
     };
     var init = function init(token, extraObj) {
-      pageToken = token;
-      console.log(extraObj);
 
       if (extraObj != null) {
         extraObj.track_pageview != undefined ? extraObj.track_pageview : false;
-        trackFirstEventInterval = extraObj.track_firsteventinterval != undefined ? extraObj.track_firsteventinterval : false;
-        console.log(trackFirstEventInterval);
+        extraObj.track_firsteventinterval != undefined ? extraObj.track_firsteventinterval : false;
       }
 
-      afterInit();
+      sendPageHitTrack(token);
     };
 
-    function afterInit() {
-      console.log("afterInit");
-
-      if (trackFirstEventInterval) {
-        initTime = new Date();
-        console.log(initTime);
-      }
+    function sendPageHitTrack(token) {
+      var params = new URLSearchParams({
+        event: 'page-hit',
+        token: token
+      });
+      var url = "".concat(urlEventTrack, "?").concat(params.toString());
+      console.log(url);
     }
 
     exports.init = init;
